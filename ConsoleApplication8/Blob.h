2699b4f7ef661882c518edce98c4668e3b4fb115
RalfRendelf <vladislavungvicky@gmail.com>
2022-10-21 16:34:48 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApplication8/Blob.h
@@ -0,0 +1,63 @@
+#pragma once
+#include <iostream>
+#include <bitset>
+#include <memory>
+#include <vector>
+#include <list>
+
+using namespace std;
+class StrBlob 
+{
+public:
+    typedef vector<string>::size_type size_type;
+
+    StrBlob();
+    StrBlob(initializer_list<string> il);
+    size_type size() const {
+        return data->size();
+    }
+    bool empty() const { return data->empty(); }
+    void push_back(const string& t) {
+       data->push_back(t);
+    }
+    void pop_back();
+    string& front();
+    string& back();
+private:
+    shared_ptr<vector<string>> data;
+    void check(size_type i, const string& msg) const;
+};
+StrBlob::StrBlob():data(make_shared<vector<string>>()){}
+StrBlob::StrBlob(initializer_list<string> il):data(make_shared<vector<string>>(il)){}
+
+
+
+
+void StrBlob::check(size_type i, const string& msg) const
+{
+    if (i >= data->size())
+        throw out_of_range(msg);
+}
+
+
+
+string& StrBlob::back()
+{
+    check(0, "back on empty StrBlob");
+    return data->back();
+
+}
+
+
+void StrBlob::pop_back()
+{
+    check(0, "pop_back on empty StrBlob");
+
+}
+
+string& StrBlob::front() 
+{
+
+    return *this;
+}
+
